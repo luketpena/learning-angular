abc2a520bedebbd054d6f9979b7bbd42dcadb1aa
Luke Peña <luketpena@gmail.com>
2020-06-10 16:36:38 -0500

    One way and two way value binding

--- src/app/binding-example/binding-example.component.ts
+++ src/app/binding-example/binding-example.component.ts
@@ -6,10 +6,32 @@ import { Component } from '@angular/core';
     <p>Please work.</p>
     <button class="btn btn-primary" [class.active]="isActive">Boostrap Button!</button>
     <button [style.backgroundColor]="isActive? 'blue' : 'white'">Style Binding!</button>
+    <button (click)="onClickButton($event)">click!</button>
+
+    <input [(ngModel)]="email" (keyup.enter)="onKeyUp()" />
   `
 })
+
+//<input #email (keyup)="onKeyUp(email.value)" />
+//#email is a template variable
 
 export class BindingExampleComponent {
   isActive = true;
   colSpan = 2;
+  email = "me@place.com";
+  
+  onClickButton($event) {
+    $event.stopPropagation(); //Prevents bubbling into parent elements
+    this.isActive = !this.isActive;
+    console.log($event);
+  }
+
+  
+
+  onKeyUp() {
+    //if ($event.keyCode===13) console.log('ENTER WAS PRESSED');
+    //console.log($event.target.value);
+
+    console.log(this.email);
+  }
 }
